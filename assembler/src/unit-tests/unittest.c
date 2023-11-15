9401ac9bfb0ebf69df7db81b3a550b66d1638eb7
Gustaf franzèn <gustaffranzen@icloud.com>
2023-11-15 17:12:41 +0100

    prepared for unit-tests

---
+++ assembler/src/unit-tests/unittest.c
@@ -0,0 +1,16 @@
+/*==========================================================*
+ *
+ * @author Gustaf Franzén :: https://github.com/BjorneEk;
+ * @author Adam Månsson
+ *
+ * unit tests
+ *==========================================================*/
+
+#include "../../../common/util/types.h"
+#include "../assembler.h"
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("UNIT TEST!\n");
+}
